d31c4618ff97a6d97b7ca4f0d7c63b0e7d3ca6d5
Paul Kukiel <kukiel@amazon.com>
2021-02-28 20:07:47 +1100

    Update comments

--- bin/cdk-wildCardStaticApp.ts
+++ bin/cdk-wildCardStaticApp.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import { wildCardStaticApp } from '../lib/wildCardStaticApp-stack';
 
 const app = new cdk.App();
 const envUS  = { 
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-1' 
 };
-new CdkWorkshopStack(app, 'CdkWorkshopStack', { env: envUS} );
+new wildCardStaticApp(app, 'wildCardStaticApp', { env: envUS} );
--- lib/wildCardStaticApp-stack.ts
+++ lib/wildCardStaticApp-stack.ts
@@ -1,51 +1,50 @@
 import cdk = require('@aws-cdk/core');
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as route53 from '@aws-cdk/aws-route53';
 import * as targets from '@aws-cdk/aws-route53-targets';
 
 const websiteDistSourcePath = './app';
-/*
-To Do
- - Add wildcard DNS for zone
- - Add Certificate
- - Atatched certificate to CF
- - Attach Alternaemcname to CF
-*/
 
-export class CdkWorkshopStack extends cdk.Stack {
+export class wildCardStaticApp extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const primaryDomain = 'randomsite.site'
     const subDomain = `*.${primaryDomain}`
 
+    // Create a private s3bucket
     const sourceBucket = new Bucket(this, 'cdk-mypoc-website-s3', {
       websiteIndexDocument: 'index.html',
-      bucketName: 'cdk-mypoc-usa'
+      bucketName: `wildacard-${primaryDomain}`
     });
 
     const OriginAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OIA', {
-      comment: "Setup Access from CF to the bucket ( read )"
+      comment: "Setup Access from CloudFront to the bucket ( read )"
     });
     sourceBucket.grantRead(OriginAccessIdentity);
 
+    // Deploy the source code from the /app folder, in this example thats just 1 file.
     new BucketDeployment(this, 'DeployWebsite', {
       sources: [Source.asset(websiteDistSourcePath)],
       destinationBucket: sourceBucket
     });
 
+    // We are using a Zone that already exists so we can use a lookup on the Zone name.
     const zone = route53.HostedZone.fromLookup(this, 'baseZone', {
       domainName: primaryDomain
     });
 
+    // Request the wildcard TLS certificate, CDK will take care of domain ownernship validation via 
+    // CNAME DNS extries in route53, a custome resourse will be used on our behalf
     const myCertificate = new acm.DnsValidatedCertificate(this, 'mySiteCert', {
       domainName: subDomain,
       hostedZone: zone,
     });
 
+    // Create the CloudFront Distribution, set the alternate CNAMEs and passin the ACM ARN of the cert created.
     const cfDist = new cloudfront.CloudFrontWebDistribution(this, 'myDist', {
       originConfigs: [
         {
@@ -64,6 +63,7 @@ export class CdkWorkshopStack extends cdk.Stack {
       }
     });
   
+    // Created the wildcard DNS entry in route53 as an aliase to the new CloudFront Distribution.
     new route53.ARecord(this, 'AliasRecord', {
       zone,
       recordName: subDomain,
